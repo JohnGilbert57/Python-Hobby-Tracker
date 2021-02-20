29f18163b7bff00df2f1ad932623ff4585f8831f
Nathan Bennett <nb566818@ohio.edu>
2021-02-20 12:30:17 -0500

    Created 3 database models, HobbyUser, Hobby, and HobbyTime

--- hobbytracker/myapp/models.py
+++ hobbytracker/myapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class HobbyUser(models.Model):
+    # Data fields
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    userName = models.CharField(max_length=200)
+    password = models.CharField(max_length=50)
+
+    # Called when displaying raw data
+    def __str__(self):
+        return self.userName + " - " + self.firstName + " " + self.lastName
+
+class Hobby(models.Model):
+    # Establish a foreign key relationship with HobbyUser
+    hobbyUser = models.ForeignKey(HobbyUser, on_delete=models.CASCADE)
+
+    # Data fields
+    name = models.CharField(max_length=300)
+    spriteId = models.IntegerField()
+
+    # Called when displaying raw data
+    def __str__(self):
+        return self.name
+
+class HobbyTime(models.Model):
+    # Establish a foreign key relationship with Hobby
+    hobby = models.ForeignKey(Hobby, on_delete=models.CASCADE)
+
+    # Data fields
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
