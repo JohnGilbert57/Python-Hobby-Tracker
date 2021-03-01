5f9a20c9b208b7e6c5bf4c67e344b4c9618dd1eb
John Gilbert <jg480318@ohio.edu>
2021-03-01 18:34:05 -0500

    fixed styling for userhobbies page

--- hobbytracker/myapp/urls.py
+++ hobbytracker/myapp/urls.py
@@ -3,10 +3,11 @@
 # how we call our functions and sets the URL routes
 from django.contrib import admin
 from django.urls import path, include
-from .views import user_login_form, new_user_form, hobbiespage
+from .views import user_login_form, new_user_form, hobbiespage, baseUrl
 from . import views
 urlpatterns = [
     path('userhobbies/task/', views.index, name = "index"),
+    path('', views.baseUrl),
     path('', include("django.contrib.auth.urls")),
     path('createaccount/', views.new_user_form, name = "register"),
     path('userhobbies/', views.hobbiespage)
