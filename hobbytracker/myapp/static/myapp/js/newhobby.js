60b42d8fe3805cd4af32d3989714f7ce105091fd
John Gilbert <jg480318@ohio.edu>
2021-03-21 16:36:21 -0400

    styling and a popup generates now

---
+++ hobbytracker/myapp/static/myapp/js/newhobby.js
@@ -0,0 +1,25 @@
+var openModalButtons = document.querySelectorAll('[data-modal-target]');
+var closeModalButtons = document.querySelectorAll('[data-close-button]');
+var overlay = document.getElementById('overlay')
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+})
+function openModal(modal){
+    if(modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+function closeModal(modal){
+    if(modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
